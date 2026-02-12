888f9f5661fa26d81b05d8c40a59cbe7efc2a171
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2026-02-12 21:23:46 +0300

    init

---
+++ src/main/java/mg/working/diakonacalendar/DiakonaCalendarApplication.java
@@ -0,0 +1,13 @@
+package mg.working.diakonacalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiakonaCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiakonaCalendarApplication.class, args);
+    }
+
+}
